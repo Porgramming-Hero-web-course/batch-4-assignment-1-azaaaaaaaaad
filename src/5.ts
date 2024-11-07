102fe2e3a49cb4b60fc0028efa94034902809e30
jrazad10 <jrazad10@gmail.com>
2024-11-07 18:52:27 +0600

    5th task done

---
+++ src/5.ts
@@ -0,0 +1,20 @@
+{
+  // poblem 5
+  const getProperty = <T, K extends keyof T>(obj: T, prop: K): T[K] => {
+    return obj[prop];
+  };
+
+  const person = {
+    name: "Alice",
+    age: 30,
+    isAdmin: true,
+  };
+
+  const name = getProperty(person, "name");
+  const age = getProperty(person, "age");
+  const isAdmin = getProperty(person, "isAdmin");
+
+  console.log(name);
+  console.log(age);
+  console.log(isAdmin);
+}
